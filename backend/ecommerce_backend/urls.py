ca7ae14d057b2c25c281b0e01ecb5a8ef15f772d
Samil Abud <samilabud@gmail.com>
2025-07-08 13:05:33 -0400

    refactor: adds image in products

--- backend/ecommerce_backend/urls.py
+++ backend/ecommerce_backend/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
+from django.conf import settings
+from django.conf.urls.static import static
 from products.views import ProductViewSet
 from orders.views   import OrderViewSet
 from orders.urls    import urlpartners as order_urls
@@ -12,8 +14,15 @@ router.register(r'orders',   OrderViewSet,   basename='order')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('auth/me/', include('users.urls')),  # your existing auth endpoint
+    path('auth/me/', include('users.urls')),
     path('api/v1/', include(router.urls)),
     path('orders/', include(order_urls)),
     path('products/', include(product_urls)),
 ]
+
+if settings.DEBUG:
+    print('DEBUG MODE')
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    # media/products/2025/07/04/home-1_CFJkrXv.jpg
+else:
+    print('PRODUCTION MODE')
